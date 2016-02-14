b30aa839b15b7cf8c54fb1b0045b53b9a723d551
qyfei <qyfei2010@gmail.com>
2016-02-14 16:31:09 +0800

    Initial commit

---
+++ src/test/java/org/qiyanfei/SeleniumTest/SeleniumTest.java
@@ -0,0 +1,43 @@
+package org.qiyanfei.SeleniumTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+public class SeleniumTest
+{
+    private  WebDriver driver;
+    private String baseUrl;
+    @BeforeClass
+    public void setUp() throws Exception {
+     //   System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+        driver = new FirefoxDriver();
+        baseUrl = "http://www.baidu.com/";
+    }
+    @Test
+    public void testSearch() throws Exception {
+        driver.get(baseUrl);
+        Thread.sleep(1000);
+        WebElement input = driver.findElement(By.id("kw"));
+        input.sendKeys("天气预报");
+        Thread.sleep(1000);
+        WebElement search = driver.findElement(By.id("su"));
+        search.click();
+        Thread.sleep(1000);
+        Assert.assertTrue(driver.getTitle().contains("天气预报"),"Title is incorrect.");
+        System.out.println(driver.getTitle());
+//      (new WebDriverWait(driver,10)).until(new ExpectedCondition<Boolean>(){
+//          public Boolean apply(WebDriver d){
+//                 return d.getTitle().startsWith("天气预报");
+//          }
+//      });
+    }
+    @AfterClass
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
